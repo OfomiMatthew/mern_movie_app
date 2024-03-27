4514123dc44a92cda7119eb015bb22fca16592b1
OfomiMatthew <ofomimatthew7@gmail.com>
2024-03-27 16:25:45 +0100

    env file configured

--- movie-backend/index.js
+++ movie-backend/index.js
@@ -1,1 +1,21 @@
-console.log(4)
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB()
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
